e0042f6bb03553738f588d2cae219c2a2615583b
deepali04 <nagwade.deepali@gmail.com>
2020-05-27 15:19:15 +0530

    Added draw

--- Game.py
+++ Game.py
@@ -22,7 +22,7 @@ screen = pg.display.set_mode((width, height+100),0,32)
 pg.display.set_caption("Tic Tac Toe")
 
 #Loading the images
-logo = pg.image.load('TicTcToeLogo.jpg')
+logo = pg.image.load('TicTacToeLogo.jpg')
 x_img = pg.image.load('x.png')
 o_img = pg.image.load('o.png')
 
@@ -55,11 +55,12 @@ def draw_status():
     if winner is None:
         message = XO.upper() + "'s Turn"
     else:
-        message = winner.upper() + " won!"
+        message = winner.upper() + " is the Winner!"
     if draw:
-        message = 'Game Draw!'
+        message = 'Game Draw!!!'
     font = pg.font.Font(None, 30)
     text = font.render(message, 1, (255, 255, 255))
+
     # copy the rendered message onto the board
     screen.fill ((0, 0, 0), (0, 400, 500, 100))
     text_rect = text.get_rect(center=(width/2, 500-50))
@@ -67,7 +68,66 @@ def draw_status():
     pg.display.update()
 
 
+def check_win():
+    global TTT, winner,draw
 
+    # check for winning rows
+    for row in range (0,3):
+        if ((TTT [row][0] == TTT[row][1] == TTT[row][2]) and(TTT [row][0] is not None)):
+            # this row won
+            winner = TTT[row][0]
+            pg.draw.line(screen, (250,0,0), (0, (row + 1)*height/3 -height/6),\
+                              (width, (row + 1)*height/3 - height/6 ), 4)
+            break
 
+    # check for winning columns
+    for col in range (0, 3):
+        if (TTT[0][col] == TTT[1][col] == TTT[2][col]) and (TTT[0][col] is not None):
+            # this column won
+            winner = TTT[0][col]
+            #draw winning line
+            pg.draw.line (screen, (250,0,0),((col + 1)* width/3 - width/6, 0),\
+                          ((col + 1)* width/3 - width/6, height), 4)
+            break
 
+    # check for diagonal winners
+    if (TTT[0][0] == TTT[1][1] == TTT[2][2]) and (TTT[0][0] is not None):
+        # game won diagonally left to right
+        winner = TTT[0][0]
+        pg.draw.line (screen, (250,70,70), (50, 50), (350, 350), 4)
+       
 
+    if (TTT[0][2] == TTT[1][1] == TTT[2][0]) and (TTT[0][2] is not None):
+        # game won diagonally right to left
+        winner = TTT[0][2]
+        pg.draw.line (screen, (250,70,70), (350, 50), (50, 350), 4)
+    
+    if(all([all(row) for row in TTT]) and winner is None ):
+        draw = True
+    draw_status()
+
+def drawXO(row,col):
+    global TTT,XO
+    if row==1:
+        posx = 30
+    if row==2:
+        posx = width/3 + 30
+    if row==3:
+        posx = width/3*2 + 30
+
+    if col==1:
+        posy = 30
+    if col==2:
+        posy = height/3 + 30
+    if col==3:
+        posy = height/3*2 + 30
+    TTT[row-1][col-1] = XO
+    if(XO == 'x'):
+        screen.blit(x_img,(posy,posx))
+        XO= 'o'
+    else:
+        screen.blit(o_img,(posy,posx))
+        XO= 'x'
+    pg.display.update()
+    #print(posx,posy)
+    #print(TTT)
