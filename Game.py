20a8da1d3093e86472504ab57411cc82b6b3fbbd
deepali04 <nagwade.deepali@gmail.com>
2020-05-27 15:21:53 +0530

    Added click function

--- Game.py
+++ Game.py
@@ -128,6 +128,52 @@ def drawXO(row,col):
     else:
         screen.blit(o_img,(posy,posx))
         XO= 'x'
+def userClick():
+    #get coordinates of mouse click
+    x,y = pg.mouse.get_pos()
+
+    #get column of mouse click (1-3)
+    if(x<width/3):
+        col = 1
+    elif (x<width/3*2):
+        col = 2
+    elif(x<width):
+        col = 3
+    else:
+        col = None
+        
+    #get row of mouse click (1-3)
+    if(y<height/3):
+        row = 1
+    elif (y<height/3*2):
+        row = 2
+    elif(y<height):
+        row = 3
+    else:
+        row = None
+    #print(row,col)
+    
+
+    if(row and col and TTT[row-1][col-1] is None):
+        global XO
+        
+        #draw the x or o on screen
+        drawXO(row,col)
+        check_win()
+        
+        
+
+def reset_game():
+    global TTT, winner,XO, draw
+    time.sleep(3)
+    XO = 'x'
+    draw = False
+    game_opening()
+    winner=None
+    TTT = [[None]*3,[None]*3,[None]*3]
+    
+
+game_opening()
     pg.display.update()
     #print(posx,posy)
     #print(TTT)
